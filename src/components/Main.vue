335a429b20a2256e2f10656bbbc2dcef40456199
Alex <108487553+Al3xramirez@users.noreply.github.com>
2025-12-16 13:35:31 -0800

    Configured Build to include shims-vue

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,8 +3,6 @@
     <section class="gallery">
       <img src="../assets/photo1.jpg" alt="Photography sample 1" />
       <img src="../assets/photo2.jpg" alt="Photography sample 2" />
-      <img src="../assets/photo3.jpg" alt="Photography sample 3" />
-      <img src="../assets/photo4.jpg" alt="Photography sample 4" />
     </section>
   </main>
 </template>
