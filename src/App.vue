7bc281040bed4562df5c958c5baaed3fe416c280
Alex <108487553+Al3xramirez@users.noreply.github.com>
2025-12-15 17:16:47 -0800

    feat: initialize Vue 3 project with Vite and TypeScript
    - Established Vite configuration and project setup.

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+  import {ref} from 'vue'
+  const name = ref('Alex')
+  const emoji = ref('✌🏻')
+</script>
+
+<template>
+  <header>
+    <span>{{emoji}}</span>
+  </header>
+
+  <main>
+    <h1>Hello, {{name}}!</h1>
+    <p>This is the beginning of my photogrhapy website demo!</p>
+  </main>
+
+  <footer>
+    <p>&copy; 2025</p>
+  </footer>
+</template>
+
+<style scoped>
+  header span{
+    font-size:3rem;
+  }
+  main h1{
+      display:block;
+  }
+  main a{
+      color: #42B883;
+  }
+  footer {
+      position:fixed;
+      bottom:0;
+      padding-bottom:20px;
+  }
+</style>
